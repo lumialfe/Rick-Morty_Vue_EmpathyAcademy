901bf6a3512818294a3b30a2d124e9d81fe2c486
LuisMi Alonso <60442261+lumialfe@users.noreply.github.com>
2023-03-08 09:57:00 +0100

    Added basic search functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,57 @@
-<script setup>
+<script>
+import SearchBar from "@/components/SearchBar.vue";
 
+export default {
+  components: {
+    SearchBar
+
+  },
+  data() {
+    return {
+      character: '',
+      characters: [],
+      url: 'https://rickandmortyapi.com/api/character/',
+    }
+  },
+  watch: {
+    character() {
+      this.searchCharacters();
+    }
+  },
+  methods: {
+    setCharacter(character) {
+      this.character = character;
+    },
+    searchCharacters() {
+      fetch(this.url + '?name=' + this.character).then(response => response.json())
+          .then(data => {
+            this.characters = data.results;
+            console.log(this.characters);
+          });
+    },
+  },
+  created() {
+
+  },
+  mounted() {
+
+  }
+}
 </script>
 
 <template>
+  <header>
+    <a href="index.html"><img class="logo" src="./assets/media/logo.png" alt="Page Logo."/></a>
+    <SearchBar v-on:search="setCharacter"/>
+  </header>
+  <div class="body-main">
+    <aside>
 
+    </aside>
+    <main>
+
+    </main>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <label for="searchBar"></label>
+  <input id="searchBar" class="header-searchbar" type="text" placeholder="Find a Character..." v-on:input="$emit('search',$event.target.value)"/>
+</template>
+
+<script>
+export default {
+  name: "SearchBar"
+}
+</script>
+
+<style scoped>
+
+</style>
