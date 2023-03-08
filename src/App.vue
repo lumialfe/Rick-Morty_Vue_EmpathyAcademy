2e9ce12acdf58221470b3ab36ad4f5091ce12fb9
LuisMi Alonso <60442261+lumialfe@users.noreply.github.com>
2023-03-08 10:25:59 +0100

    Completed character card info

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
     },
   },
   created() {
-
+    this.searchCharacters();
   },
   mounted() {
 
@@ -64,10 +64,57 @@ export default {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    align-items: center;
-  }
+    align-items: center; }
 
   .logo {
-    width: 8rem;
-  }
+    width: 8rem; }
+
+  .body-main {
+    grid-area: bodymain;
+    display: grid;
+    grid-template-columns: 10% auto;
+    grid-column-gap: 1vw;
+    grid-row-gap: 1vw;
+    padding: 2vh 1vw; }
+
+  aside {
+    text-align: left;
+    background-color: #efdfd4;
+    padding: 1vh 1vw;
+    border-radius: 25px;
+    height: fit-content; }
+
+  main {
+    display: grid;
+    grid-template-columns: repeat(5, minmax(50px, 1fr));
+    grid-row-gap: 2vh;
+    grid-column-gap: 1vw; }
+
+
+  @media only screen and (max-width: 640px) {
+    main {
+      grid-template-columns: repeat(2, minmax(50px, 1fr));
+      grid-column-gap: 2vw;
+      grid-row-gap: 1vh; }
+
+    .body-main {
+      grid-template-columns: 100%; }
+
+    aside {
+      display: none; }
+
+    body {
+      grid-template-rows: 1fr auto auto;
+      grid-template-areas: "header" "mobilefilters" "bodymain"; }
+
+    .header-dropdown {
+      grid-area: mobilefilters;
+      display: block; }
+
+    .body-mobile-filters {
+      display: none; }
+
+    .card-image {
+      max-width: 90%;
+      margin: 0px auto auto; } }
 </style>
--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -13,6 +13,16 @@
 
   <div class='main-card'>
     <img class='card-image' loading='lazy' v-bind:src="character.image" v-bind:alt="character.name"/>
+    <p class="card-name">{{character.name}}</p>
+    <p class="card-text">
+      Status: {{character.status}}
+      <br/>
+      Gender: {{character.gender}}
+      <br/>
+      Species: {{character.species}}
+      <br/>
+      Origin: {{character.origin.name}}
+    </p>
   </div>
 </template>
 
@@ -23,5 +33,24 @@ export default {
 </script>
 
 <style scoped>
+  .main-card {
+    padding: 2vh 1vw;
+    background-color: white;
+    justify-content: center;
+    display: grid;
+    grid-template-areas: "image" "name" "text";
+    border-radius: 25px; }
 
+  .card-image {
+    grid-area: image;
+    width: 100%;
+    border-radius: 25px;
+    margin-top: 1vh; }
+
+  .card-name {
+    grid-area: name;
+    font-size: 150%; }
+
+  .card-text {
+    grid-area: text; }
 </style>
