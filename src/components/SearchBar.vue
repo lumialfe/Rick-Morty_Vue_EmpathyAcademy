beee1afbf4dfc8014e807644ec659f3b4e6fcffb
LuisMi Alonso <60442261+lumialfe@users.noreply.github.com>
2023-03-10 10:42:23 +0100

    Changed SearchBar.vue Rendering

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,8 @@
 <template>
   <label for="searchBar"></label>
-  <input id="searchBar" class="header-searchbar" type="text" placeholder="Find a Character..." v-on:input="$emit('search',$event.target.value)"/>
+  <input id="searchBar" class="header-searchbar" type="text" placeholder="Search..."
+         onfocus="this.placeholder=''" onblur="this.placeholder='Search...'"
+          v-on:input="$emit('search',$event.target.value)"/>
 </template>
 
 <script>
@@ -11,7 +13,7 @@ export default {
 
 <style scoped lang="scss">
 .header-searchbar {
-  width: 40vw;
+  width: 4rem;
   height: auto;
   padding: 1rem 2rem;
   border: none;
@@ -20,5 +22,23 @@ export default {
   background: url("../assets/media/search-icon.svg") no-repeat 15px center;
   background-size: 1rem;
   font-family: "Montserrat", sans-serif;
+  transition: width .5s ease-in-out;
+}
+
+*:focus {
+  outline: none;
+  transition: width .5s ease-in-out;
+  width: 15vw;
+}
+
+@media only screen and (max-width: 640px) {
+  .header-searchbar {
+    width: 35%;
+  }
+  *:focus {
+    outline: none;
+    transition: width .5s ease-in-out;
+    width: 35%;
+  }
 }
 </style>
