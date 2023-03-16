ff3437c3d57eb936dc9b0e31d8ce40dd5e446688
LuisMi Alonso <60442261+lumialfe@users.noreply.github.com>
2023-03-16 10:38:33 +0100

    Preparing for Deploy II

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue';
-import {createStore} from 'vuex';
-import App from './App.vue';
-
-import {store} from "@/store/store";
-
-import './assets/styles/style.css';
-
-createApp(App).use(store).mount('#app');
